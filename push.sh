e41927d4b14f10308330b33737e216a00b4890d6
mrCrendel <tggrmi@gmail.com>
2019-03-21 14:58:49 +0600

    asdasd

--- push.sh
+++ push.sh
@@ -4,4 +4,4 @@ git add .
 git commit -m "$1"
 git push origin master https://github.com/mrCrendel/online-shop.git
 Username for 'https://github.com': 'mrCrendel'
-Password for 'https://mrCrendel@github.com':
+Password for 'https://mrCrendel@github.com': 
