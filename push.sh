6ec00976286ef219d8d89a2663ee4d49f7b5e377
mrCrendel <tggrmi@gmail.com>
2019-03-21 14:42:06 +0600

    asdasd

--- push.sh
+++ push.sh
@@ -2,4 +2,4 @@
 
 git add .
 git commit -m "$1"
-git push origin master git+ssh://git@github.com/mrCrendel/online-shop.git
+git push remote origin master git+ssh://git@github.com/mrCrendel/online-shop.git
