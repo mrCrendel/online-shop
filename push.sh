26e676743d56ce03063c656929e956bedac3b1d4
mrCrendel <tggrmi@gmail.com>
2019-03-21 15:18:43 +0600

    asdasd

--- push.sh
+++ push.sh
@@ -5,6 +5,6 @@ git commit -m "$1"
 #git push origin master https://github.com/mrCrendel/online-shop.git
 ##git remote set-url origin git@github.com:mrCrendel/online-shop.git
 git remote set-url origin git@github.com:mrCrendel/online-shop.git
-#Username for 'https://github.com': mrCrendel
-#Password for 'https://xyz@github.com': t3erob1nbd_t
+Username for 'https://github.com': mrCrendel
+Password for 'https://xyz@github.com': t3erob1nbd_t
 git config --global credential.helper cache
